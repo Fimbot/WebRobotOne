38e351ce559fa554c3f63019a47fb14280e20b79
Fimbot <58072168+Fimbot@users.noreply.github.com>
2020-01-26 10:43:23 +0100

    Add files via upload

---
+++ webfimbot/src/main/java/com/example/webfimbot/WebfimbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfimbot;
+//555
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfimbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfimbotApplication.class, args);
+    }
+
+}
---
+++ webfimbot/src/test/java/com/example/webfimbot/WebfimbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfimbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfimbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
